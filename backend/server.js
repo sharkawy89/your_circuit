09eacba6e9e86915128b2fe4c05f40e4eb4cbafb
Adham sharkawy <adhamsharkawy185@gmail.com>
2025-12-08 21:29:32 +0200

    dd

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: 'http://next-circuit.vercel.app',
+    origin: 'https://your-circuit.vercel.app/',
     credentials: true
 }));
 app.use(morgan('dev'));
